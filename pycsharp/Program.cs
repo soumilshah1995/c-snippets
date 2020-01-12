2eadbbf0fd6b92871e30e415b430075ff7fc298f
soumilshah1995 <soushah@my.bridgeport.edu>
2020-01-11 22:55:17 -0500

    added pycharp

---
+++ pycsharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO; 
+using System.Diagnostics; 
+
+namespace pycsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileName = "pyscripts.py"; 
+            ProcessStartInfo ProcessInfo = new ProcessStartInfo("python3"); 
+
+            // make sure we can read the output from stdout 
+            ProcessInfo.UseShellExecute = false; 
+            ProcessInfo.RedirectStandardOutput = true; 
+
+            ProcessInfo.Arguments = FileName;
+            Process myProcess = new Process(); 
+
+            // assign start information to the process 
+            myProcess.StartInfo = ProcessInfo; 
+
+            // start the process 
+            myProcess.Start(); 
+
+            StreamReader myStreamReader = myProcess.StandardOutput; 
+            string myString = myStreamReader.ReadLine(); 
+            myProcess.WaitForExit(); 
+            myProcess.Close();
+            Console.WriteLine("Value received from script: " + myString); 
+        
+
+        }
+    }
+}
---
+++ pycsharp/pyscripts.py
@@ -0,0 +1,17 @@
+import sys
+import json
+import requests
+
+url = "https://opensourcepyapi.herokuapp.com:443/weather/06604"
+r = requests.get(url)
+data = r.json()
+
+resp = {
+    "Response":200,
+    "Message":"Data from Python ",
+    "Data":data
+}
+print(json.dumps(resp))
+
+sys.stdout.flush()
+
