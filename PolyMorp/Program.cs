a45e836b9c662a4b1bd77e9b84d985094821ebdc
soumilshah1995 <soushah@my.bridgeport.edu>
2020-01-13 12:54:27 -0500

    added Polymorphism example

---
+++ PolyMorp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PolyMorp
+{
+
+    public class Shape
+    {
+        public Shape()
+        {
+            System.Console.WriteLine("Shape  Constructor ..........");
+        }
+
+        // Virtual method
+        public virtual void Draw()
+        {
+            Console.WriteLine("Performing base class drawing tasks");
+        }
+    }
+
+    class Circle : Shape
+    {
+        public Circle():base()
+        {
+            System.Console.WriteLine("Circle class Constructor ......");
+        }
+
+        public override void Draw()
+        {
+            // Code to draw a circle...
+            Console.WriteLine("Drawing a circle");
+            // base.Draw();
+        }
+    }
+
+    public class Rectangle:Shape{
+        public Rectangle()
+        {
+            System.Console.WriteLine("Rectangle class Constructor .........");
+        }
+
+        public override void  Draw()
+        {
+            System.Console.WriteLine("Drawing Rectangle .........");
+        }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Condition for Polymorphism 
+            //  It should be Base class
+            // Method should be mark as virtual 
+            // it should be overrided in Child Class 
+
+            
+           Shape f2 = new Circle();
+           f2.Draw();
+
+
+        }
+    }
+}
