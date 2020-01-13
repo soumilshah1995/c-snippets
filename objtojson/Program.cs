d096f109d002f164cf8ec5b6e4657ff7edc809cb
soumilshah1995 <soushah@my.bridgeport.edu>
2020-01-12 19:02:01 -0500

    added code for object to json and vice versa

---
+++ objtojson/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace objtojson
+{
+    class Person{
+        public string FirstName 
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public Dates dates
+        {
+            get;
+            set;
+        }
+
+
+    }
+
+    class Dates
+    {
+        public string Birthday
+        {
+            get;
+            set;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var obj = new Person{
+                FirstName = "Soumil",
+                LastName = "Shah",
+                dates = new Dates
+                {
+                    Birthday = "12/08/1995"
+                }
+            };
+  
+            var jsonString = JsonConvert.SerializeObject(obj);
+            System.Console.WriteLine(jsonString);
+            
+            System.Console.WriteLine("=========");
+            
+            dynamic json  = JsonConvert.DeserializeObject(jsonString);
+            System.Console.WriteLine(json);
+        }
+    }
+}
