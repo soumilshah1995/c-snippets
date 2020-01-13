496f823c7956cd67d5944fdc82ba0ba7b10a1389
soumilshah1995 <soushah@my.bridgeport.edu>
2020-01-13 12:31:07 -0500

    added Singelton Design pattern

---
+++ mySingelton/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace mySingelton
+{
+
+    class Foo{
+        private Foo(){}
+        
+        private static Foo _instance;
+
+        public static Foo getInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Foo();
+            }
+            
+            return _instance;
+            
+        }
+
+        public void SomeFunc()
+        {
+            System.Console.WriteLine("Some code ");
+        }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo obj  = Foo.getInstance();
+            obj.SomeFunc();
+
+        }
+    }
+}
